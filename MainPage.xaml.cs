d1b5801609e43d23719810ac8463161bac0872b1
VDiola <126273351+VDiola@users.noreply.github.com>
2023-10-09 08:07:30 +0800

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace MauiApp3;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnDial_Clicked(object sender, EventArgs e)
+	{
+		try
+		{
+			PhoneDialer.Open(PhoneNum.Text);
+
+        }
+		catch
+		{
+			await DisplayAlert("Invalid Input", "Please enter a valid phone number", "OK");
+		}
+	}
+}
+
